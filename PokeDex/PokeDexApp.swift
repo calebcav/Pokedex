e5600c077b205b55f310c41625d2ea1799a046d1
calebcav <cavinesscaleb@gmail.com>
2024-10-13 13:25:26 -0500

    created repo

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Caleb Caviness on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokeDexTests/PokeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexTests.swift
+//  PokeDexTests
+//
+//  Created by Caleb Caviness on 10/13/24.
+//
+
+import Testing
+@testable import PokeDex
+
+struct PokeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
