650964e72eeed74bf0bc76c4fbbbd4d860f0a062
calebcav <cavinesscaleb@gmail.com>
2024-10-20 18:48:08 -0500

    created about view

---
+++ PokeDex/PokemonAboutView.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonAboutView.swift
+//  Pokedex
+//
+//  Created by Caleb Caviness on 10/20/24.
+//
+
+import SwiftUI
+
+struct PokemonAboutView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack
+        }
+    }
+}
+
+#Preview {
+    PokemonAboutView()
+}
